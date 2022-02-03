09c9a9a1e3652d97d98571f7bd139617ca2e4540
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-03 16:51:42 -0700

    Fix descriptiion on spec test

---
+++ spec/features/frog/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'frog show page', type: :feature do
+  before do
+    @bog = Bog.create!(name: "Just a bog", ph: 5.6, radioactive: false)
+
+    @tony = @bog.frogs.create!(name: "Anthony Hopkins", age: 15, singing: false)
+  end
+
+  it 'displays all data for one frog' do
+    visit "/frogs/#{@tony.id}"
+
+    expect(page).to have_content("Meet #{@tony.name}!")
+    expect(page).to have_content(@tony.age)
+    expect(page).to have_content(@tony.singing)
+  end
+end
