dc95771acb94fb576d582d5b8621983de41da8d5
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-04 16:35:00 -0700

    Pass frog count for one bog

--- spec/features/bogs/show_spec.rb
+++ spec/features/bogs/show_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe 'Bog show', type: :feature do
     expect(page).to have_content(@toxic_waste.ph)
     expect(page).to have_content(@toxic_waste.radioactive)
   end
+
+  it 'counts all frogs in the bog' do
+    frog1 = @toxic_waste.frogs.create!(name: "Ribbit Downey Jr", age: 5, singing: true)
+    frog2 = @toxic_waste.frogs.create!(name: "Frogger", age: 4, singing: false)
+    frog3 = @toxic_waste.frogs.create!(name: "Toady", age: 5, singing: true)
+    frog4 = @toxic_waste.frogs.create!(name: "Demi Lovatoad", age: 10, singing: true)
+    visit "bogs/#{@toxic_waste.id}"
+
+    expect(page).to have_content("Frog population for this bog: 4")
+  end
 end
